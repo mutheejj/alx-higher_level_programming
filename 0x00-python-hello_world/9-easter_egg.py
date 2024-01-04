b1807967a5a543e0e2bc7945e866b698be273d4b
mutheejj <johnmuthee547@gmail.com>
2024-01-04 14:40:07 -0800

    l

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,6 +1,5 @@
 #!/bin/bash
-egg = """
-The Zen of Python, by Tim Peters
+print("""The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -21,5 +20,4 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
-"""
-print(egg)
+""")
