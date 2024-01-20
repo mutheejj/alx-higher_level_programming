35654fb00a7ccbba591ed74269c0047f36c869f7
mutheejj <johnmuthee547@gmail.com>
2024-01-20 01:44:52 -0800

    l

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = validate_tuple(tuple_a)
+    tuple_b = validate_tuple(tuple_b)
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+
+
+def validate_tuple(_tuple=()):
+    if len(_tuple) < 2:
+        if len(_tuple) == 1:
+            _tuple = (_tuple[0], 0)
+        elif len(_tuple) == 0:
+            _tuple = (0, 0)
+    elif len(_tuple) > 2:
+        _tuple = (_tuple[0], _tuple[1])
+
+    return _tuple
