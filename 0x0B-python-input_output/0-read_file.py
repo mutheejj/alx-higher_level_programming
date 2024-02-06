a08b861295b8523e74960b24e5573cbb5eccb48f
mutheejj <johnmuthee547@gmail.com>
2024-02-06 11:28:05 -0800

    l

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
+
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
