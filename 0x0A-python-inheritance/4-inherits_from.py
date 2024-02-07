3f0f2958efed758a0b5719a8c7585beb9ec32a8b
mutheejj <johnmuthee547@gmail.com>
2024-02-07 11:45:03 -0800

    l

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-       issubclass(a_class, obj.__class__) is False:
-        return True
+"""Module containing inherits_from method"""
 
-    return False
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
