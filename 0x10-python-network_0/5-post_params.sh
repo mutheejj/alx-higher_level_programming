98a3b8e125e7ac2436e2abee4aae2c6d13f8232f
mutheejj <johnmuthee547@gmail.com>
2024-04-25 13:58:23 +0300

    l

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #..
-curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+curl -s -d "email=hrtest.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
