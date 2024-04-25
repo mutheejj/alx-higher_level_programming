5f46c5079e2a3911b13f0831e766e8297ee9e393
mutheejj <johnmuthee547@gmail.com>
2024-04-25 13:52:35 +0300

    l

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -3,4 +3,10 @@
 email="test@gmail.com"
 subject="I will always be here for PLD"
 
+
+echo "POST params:"
+printf "    email: %s\n" "$email"
+printf "    subject: %s\n" "$subject"
+
+
 curl -s "$1" -X POST -d "email=$email&subject=$subject"
