30c069ab53af945b1f412dbc7e2bc0f9ecce4d90
mutheejj <johnmuthee547@gmail.com>
2024-04-26 16:58:29 +0300

    l

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Takes   m(username and password)
+and uses the Github API to display my Github id.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2])).json()
+
+    print(req.get('id'))
